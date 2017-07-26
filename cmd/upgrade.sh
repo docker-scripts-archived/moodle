dc3904d45a4a4a84c045a680b6b8b18b645c6db9
Dashamir Hoxha <dashohoxha@gmail.com>
2017-07-26 18:29:20 +0200

    Upgrade Moodle to the latest version.

---
+++ cmd/upgrade.sh
@@ -0,0 +1,28 @@
+cmd_upgrade_help() {
+    cat <<_EOF
+    upgrade
+        Upgrade to the latest version of Moodle.
+
+_EOF
+}
+
+cmd_upgrade() {
+    # backup
+    ds backup
+
+    # reinstall
+    ds remove
+    ds build
+    ds create
+    ds config
+    ds restart
+
+    # restore
+    local datestamp=$(date +%F)
+    local backup_file=backup-$CONTAINER-$datestamp.tgz
+    ds restore $backup_file
+    sleep 3
+
+    # run upgrade script
+    ds exec php admin/cli/upgrade.php --non-interactive
+}
