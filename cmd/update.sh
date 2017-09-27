035af3875a1b7c927d8952ccacb79857a9681fcc
Dashamir Hoxha <dashohoxha@gmail.com>
2017-09-27 17:06:57 +0200

    Fix update to handle clones as well.

--- cmd/update.sh
+++ cmd/update.sh
@@ -1,54 +1,14 @@
 cmd_update_help() {
     cat <<_EOF
-    update
-        Update Moodle.
+    update [<target>]
+        Update Moodle. <target> can be 'moodle' (default) or
+        the name of a clone like: 'moodle_test', 'moodle_01', etc.
 
 _EOF
 }
 
 cmd_update() {
-    local php='ds exec sudo --user=www-data php'
-    local git='ds exec sudo --user=www-data git'
-    local mysql='mysql --defaults-file=/etc/mysql/debian.cnf'
-
-    output() { echo -e "\n--> $1..."; }
-
-    output 'Run cron'
-    $php admin/cli/cron.php | grep 'task failed:'
-
-    output 'Enable maintenance mode'
-    $php admin/cli/maintenance.php --enable
-
-    output 'Purge caches'
-    $php admin/cli/purge_caches.php
-
-    output 'Make a full backup'
-    ds backup +data
-    ls -l backup*
-
-    local ans
-    read -p "Continue with update? [Y/n]: " ans
-    ans=${ans:-y}
-    ans=${ans,}
-    if [[ $ans == 'y' ]]; then
-        output 'Git pull'
-        $git stash
-        $git pull
-        $git stash pop
-        $git diff
-
-        output 'Run upgrade script'
-        $php admin/cli/upgrade.php --non-interactive
-
-        output 'Purge caches'
-        $php admin/cli/purge_caches.php
-    else
-        echo "Update aborted."
-    fi
-
-    output 'Disable maintenance mode'
-    $php admin/cli/maintenance.php --disable
-
-    output 'Run cron'
-    $php admin/cli/cron.php | grep 'task failed:'
+    local target=${1:-moodle}
+    [[ $target == 'moodle' ]] && ds backup +data
+    ds runcfg dev/update $target
 }
