40d149b097eeb97efd9c020aff9eed0ec90d8729
Dashamir Hoxha <dashohoxha@gmail.com>
2017-09-29 18:26:14 +0200

    Install automatically the latest version of plugins. (#3)

--- cmd/install-plugins.sh
+++ cmd/install-plugins.sh
@@ -1,18 +1,20 @@
 cmd_install-plugins_help() {
     cat <<_EOF
-    install-plugins
+    install-plugins [<plugin>...]
         Install Moodle additional plugins.
+        With no arguments installs the PLUGINS from 'settings.sh'
 
 _EOF
 }
 
 cmd_install-plugins() {
-    [[ -n $PLUGINS ]] || return
+    local plugins="$@"
+    [[ -n $plugins ]] || plugins="$PLUGINS"
+    [[ -n $plugins ]] || fail "Usage:\n$(cmd_install-plugins_help)"
 
-    for plugin in $PLUGINS; do
-        ds runcfg plugin/$plugin
-    done
+    # install plugins
+    ds runcfg dev/install-plugins $plugins
 
-    # cleanup
-    ds cc
+    # config plugins
+    ds runcfg plugin/offlinequiz
 }
