c10f9f27f8e1f86813b4ec7d2458cc1fb849eab4
Dashamir Hoxha <dashohoxha@gmail.com>
2017-10-29 22:14:19 +0100

    Kepping cache on ram can exhaust ram and degrade the performance.

--- scripts/cfg/01_mount_tmp_on_ram.sh
+++ scripts/cfg/01_mount_tmp_on_ram.sh
@@ -8,8 +8,8 @@ cat <<_EOF >> /etc/fstab
 ### appended by installation scripts
 tmpfs	 /dev/shm                 tmpfs    defaults,noexec,nosuid               0    0
 devpts	 /dev/pts                 devpts   rw,noexec,nosuid,gid=5,mode=620      0    0
-tmpfs    /host/data/cache         tmpfs    defaults,noatime,mode=1777,nosuid    0    0
-tmpfs    /host/data/localcache    tmpfs    defaults,noatime,mode=1777,nosuid    0    0
+# tmpfs    /host/data/cache         tmpfs    defaults,noatime,mode=1777,nosuid    0    0
+# tmpfs    /host/data/localcache    tmpfs    defaults,noatime,mode=1777,nosuid    0    0
 _EOF
 
 mount -a
